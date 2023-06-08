c2db3eed5abcddaaf90d5dd32a246cf5284ce78a
makerboomRijnland <makerboom@mborijnland.nl>
2023-06-08 13:03:51 +0200

    Tweeter OOP

--- tweeter.js
+++ tweeter.js
@@ -1,43 +1,62 @@
-let tweets = [];
-let tweetsRendered = 0;
+class Tweet {
+    constructor(data) {
+        this.displayName = data.displayName;
+        this.userName = data.userName;
+        this.postedAt = data.postedAt;
+        this.content = data.content;
+        this.image = data.image;
+    }
 
-function createTweet(tweet) {
-    const tweetsSection = document.getElementById('tweets');
+    timeAgo() {
+        const now = Date.now();
+        const date = new Date(this.postedAt);
+        
+        const msAgo = now - date;
+        const minutesAgo = Math.round(msAgo / 1000 / 60);
+        
+        if(minutesAgo < 60) {
+            return `${minutesAgo}m`;
+        }
 
-    const templateElement = document.getElementById('tweet-template');
-    const tweetArticle = templateElement.content.cloneNode(true);
+        const hoursAgo = Math.round(minutesAgo / 60);
+        if(hoursAgo < 24) {
+            return `${hoursAgo}h`;
+        }
 
-    tweetArticle.querySelector('.displayName').innerHTML = tweet.displayName;
-    tweetArticle.querySelector('.userName').innerHTML = tweet.userName;
-    tweetArticle.querySelector('.timeAgo').innerHTML = tweet.timeAgo;
-    tweetArticle.querySelector('.content').innerHTML = tweet.content;
-    tweetArticle.querySelector('.image').src = tweet.image;
+        const daysAgo = Math.round(hoursAgo / 24);
+        return `${daysAgo}d`;
+    }
 
-    tweetsSection.appendChild(tweetArticle);
-}
+    html() {
+        const tweetsSection = document.getElementById('tweets');
 
-function parseTweets(tweets) {
-    for (let index = 0; index < tweets.length; index++) {
-        const tweet = tweets[index];
-        createTweet(tweet);
+        /** @type {HTMLTemplateElement} */
+        const templateElement = document.getElementById('tweet-template');
+        /** @type {HTMLElement} */
+        const tweetArticle = templateElement.content.cloneNode(true);
+
+        tweetArticle.querySelector('.displayName').innerHTML = this.displayName;
+        tweetArticle.querySelector('.userName').innerHTML = this.userName;
+        tweetArticle.querySelector('.timeAgo').innerHTML = this.timeAgo();
+        tweetArticle.querySelector('.content').innerHTML = this.content;
+        tweetArticle.querySelector('.image').src = this.image;
+
+        tweetsSection.appendChild(tweetArticle);
     }
 }
 
-const testTweet = {
-    displayName: "John",
-    userName: "@JohnWest",
-    timeAgo: "15h",
-    content: "Test Tweet!",
-    image: "https://picsum.photos/200/300"
-};
-
-// createTweet(testTweet);
-
-// TODO: On Load uitvoeren!
-function setup() {
-    fetch("./tweets.json")
+class TweetList {
+    constructor() {
+        fetch("./tweets.json")
         .then((response) => response.json())
-        .then(parseTweets);
+        .then((data) => this.parse(data));
+    }
+
+    parse(data) {
+        document.getElementById('tweets').innerHTML = "";
+        this.tweets = data.map((data) => new Tweet(data));
+        this.tweets.forEach((tweet) => tweet.html());
+    }
 }
 
-window.addEventListener('load', setup);
+const tweetList = new TweetList();
